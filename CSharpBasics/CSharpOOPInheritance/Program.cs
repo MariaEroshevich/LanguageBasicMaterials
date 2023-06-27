f62fe5691d3aafacf274e28aa4366e92c7d0fbb9
Liliya Peravozchykava <lily.brightlight@mail.ru>
2023-06-27 22:03:14 +0300

    LINQ (1st part)

--- CSharpBasics/CSharpOOPInheritance/Program.cs
+++ CSharpBasics/CSharpOOPInheritance/Program.cs
@@ -2,38 +2,46 @@
 using CSharpOOPInheritance.AbstractClass;
 using CSharpOOPInheritance.Interface;
 
-//Bike bike = new Bike("Bike", "Aist", 55.25, "pink");
+static class Program
+{
+    static void Main()
+    {
+        
+        //Bike bike = new Bike("Bike", "Aist", 55.25, "pink");
 
-//bike.GetFuelType();
+        //bike.GetFuelType();
 
-//Console.WriteLine(bike.Mark);
+        //Console.WriteLine(bike.Mark);
 
-//bike.Move();
+        //bike.Move();
 
-//Console.WriteLine(bike.Color);
+        //Console.WriteLine(bike.Color);
 
-//Transport transport = new Transport();
-//transport.GetFuelType();
+        //Transport transport = new Transport();
+        //transport.GetFuelType();
 
-//Bike bike = new Bike("Bike", "Aist", 55.25, "pink");
-////bike.Move();
-//Console.WriteLine(bike.TransportType);
+        //Bike bike = new Bike("Bike", "Aist", 55.25, "pink");
+        ////bike.Move();
+        //Console.WriteLine(bike.TransportType);
 
-//Transport transport = new Bike();
-//transport.GetFuelType();
-//transport.GetColor();
-
-
-//AirPlane plane = new AirPlane("plane", "boing", 1880, "white", "disel");
-////plane.GetColor();
-////plane.GetFuelType();
-////plane.PrintInformation();
-//Console.WriteLine(plane.TransportType);
-
-Car car = new Car("drive");
-car.Run();
+        //Transport transport = new Bike();
+        //transport.GetFuelType();
+        //transport.GetColor();
 
 
-Bird bird = new Bird();
-bird.Move();
+        //AirPlane plane = new AirPlane("plane", "boing", 1880, "white", "disel");
+        ////plane.GetColor();
+        ////plane.GetFuelType();
+        ////plane.PrintInformation();
+        //Console.WriteLine(plane.TransportType);
+
+        Car car = new Car("drive");
+        //car.Run();
+
+
+        Bird bird = new Bird();
+        bird.Move();
+    }
+}
+
 
---
+++ CSharpBasics/LINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using CSharpOOPAccessModifiers;
+
+static class Program
+{
+    static void Main()
+    {
+        // Where
+        var animals = new List<string> { "Zebra", "Elephant", "Cat", "Dog", "Lion", "Giraffe", "Parrot" };
+        var modifiedAnimals = ChangeCollection(animals);
+        var modifiedAnimalsLinq =
+            animals.Where(animal => animal.Contains("o"));//.Select(name => name.Replace("o", "a")).ToList();
+        var modifiedAnimalsLinqImmediate = modifiedAnimalsLinq.ToList();
+
+        // Select
+        var products = new List<Milk>();
+        products.Add(new Milk("Savushkin", 3));
+        products.Add(new Milk("Prostokvashino", 2));
+        products.Add(new Milk("Brest-Litovsk", 1));
+
+        var milkPercentages = products.Select(m => m.Percent).ToList();
+
+        // Linq
+        var milkList = milkPercentages.Select(percentage => new Milk(percentage)).ToList();
+
+        // Equal to Linq
+        var milkList1 = new List<Milk>();
+        foreach (var percentage in milkPercentages)
+        {
+            var milk = new Milk(percentage);
+            milkList1.Add(milk);
+        }
+
+        // Difference
+        var milkListSelect = milkPercentages.Select(percentage => percentage < 2).ToList();
+        var milkListWhere = milkPercentages.Where(percentage => percentage < 2).ToList();
+
+        Console.WriteLine(string.Join(',', milkPercentages));
+
+    }
+    // (x, y, z) => x*y*z
+    public static void HelloWorld() => Console.Write("Hello world");
+
+    public static List<string> ChangeCollection(List<string> initialCollection, char oldChar = 'o', char newChar = 'a')
+    {
+        var modifiedCollection = new List<string>();
+        foreach (var animal in initialCollection)
+        {
+            string modifiedItem = animal.Replace(oldChar, newChar);
+            modifiedCollection.Add(modifiedItem);
+        }
+        return modifiedCollection;
+    }
+}
